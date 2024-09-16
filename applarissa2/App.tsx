f80edb9109dd5b691eba4736cc6a2f8b5ea7aa56
FormalHat <159553145+l4riss2@users.noreply.github.com>
2024-09-16 11:42:50 -0300

    gittttttttttttttttttttttt
    gittttttttttttttttttttttttttttttt

---
+++ applarissa2/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+export default function App () {
+  return(
+      <View>
+        <View>
+            <Text>Box1</Text>
+        </View>
+        <View>
+          <Text>Box1</Text>
+        </View>
+      </View>
+
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+    container :{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor : '#f0f0f0',
+    },
+    box1:{
+        width:100,
+        height:100,
+        backgroundColor : '#f0f0f0',
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:20,
+    },
+    box2:{
+        width:100,
+        height:100,
+        backgroundColor : 'lightcoral',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+  
+})
